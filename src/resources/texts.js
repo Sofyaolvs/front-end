252084c85b598c7c32734cc7356207062b54ab00
sofya <sofyaoliveira100@gmail.com>
2026-01-09 11:12:29 -0300

    ajuste texto

--- src/resources/texts.js
+++ src/resources/texts.js
@@ -20,11 +20,8 @@ export const texts = {
       "Pedale por Fortaleza",
       "Onde encontrar",
       "Dados e Conteúdos",
-
-      
       "Sobre o projeto",
       "Fale conosco",
-
     ],
   },
 
@@ -36,7 +33,7 @@ export const texts = {
       heroSubheader:
         "O Bora de Bike incentiva o hábito saudável de se locomover pela cidade",
       contentSection: [
-        "Um projeto feito pelos cidadãos, para os cidadãos",
+        "Um projeto feito para os cidadãos",
         "Como estilo de vida ou meio de transporte, escolher a bike ajuda a levar nossa cidade a um futuro mais sustentável, saudável e agradável. Já evoluímos muito, mas te convidamos a descobrir e conhecer Fortaleza sob duas rodas.",
         "Explore tudo o que nossa cidade oferece",
         "Encontre aqui a nossa malha cicloviária, bicicletários, estações do Bicicletar e muito mais.",
@@ -78,15 +75,16 @@ export const texts = {
       heroSubheader:
         "Um projeto da Prefeitura de Fortaleza para incentivar o hábito saudável de se locomover pela cidade",
       contentSection: [
-        "A evolução da política cicloviária de Fortaleza",
+        "A evolução histórica e um novo olhar para a bicicleta em Fortaleza",
         "O portal Bora de Bike, parte da nova política cicloviária de Fortaleza, consolida as ações relacionadas à ciclomobilidade na capital cearense. Essa recente iniciativa é fruto de um novo olhar para o desenvolvimento da política cicloviária e do planejamento urbano focado para gerar mais inclusão e conforto para quem deseja viver também  a cidade em cima da bicicleta.",
         "Apesar dos avanços no passado, pedalar na nossa cidade ainda pode ser um desafio para determinados públicos, que não sentem a devida segurança ao subir na bicicleta, seja pela qualidade e proteção da infraestrutura disponível ou pela falta de conhecimento de iniciativas voltadas à promoção da ciclomobilidade. Para resgatar o caráter democrático da cultura ciclável, reforçando a visão de que a cidade precisa ser planejada para todos, o Bora de Bike reforça o convite para que mais e mais pessoas possam se sentir à vontade para experimentar e viver Fortaleza através do pedal.",
         "Conheça um pouco dessa história e venha fazer parte deste novo ciclo.",
         "Histórico de ações",
-        "Em 2013, a infraestrutura urbana para ciclistas contava com 68 km de ciclovias isoladas e dados expressivos de acidentes envolvendo bicicletas, com cerca de 370 mortes por ano. Logo em seguida veio a primeira Política de Transporte Cicloviário (PTC) e o Plano Diretor Cicloviário Integrado (PDCI) em 2014, na Câmara Municipal de Fortaleza. No mesmo ano, foi formado o Plano Ação Imediatas de Trânsito e Transportes (PAITT), equipe montada na Secretaria Municipal da Conservação e Serviços Públicos (SCSP) para promover projetos de mobilidade sustentável, com atenção especial para o transporte por bicicleta.",
+        "Em 2013, a infraestrutura urbana para ciclistas contava com 68 km de ciclovias isoladas e dados expressivos de acidentes envolvendo bicicletas, com cerca de 370 mortes por ano. Logo em seguida veio a primeira Política de Transporte Cicloviário (PTC) e o Plano Diretor Cicloviário Integrado (PDCI) em 2014, na Câmara Municipal de Fortaleza. A partir do mesmo ano, uma equipe montada na Secretaria Municipal da Conservação e Serviços Públicos (SCSP) passou a promover projetos de mobilidade sustentável, com atenção especial para o transporte por bicicleta.",
+        "Com os últimos avanços da política cicloviária da Capital, Fortaleza foi apontada em 2025, pela primeira vez na história, pelo Copenhagenize – índice global da Copenhagenize Design Company – como a terceira cidade mais amigável da América Latina para se andar de bicicleta. A cidade ainda ficou com a 69ª colocação no ranking mundial do índice, conquistando um marco histórico para a cultura do ciclismo local.",
         "Ações desenvolvidas",
         "A Gerência de Gestão Cicloviária na Autarquia Municipal de Trânsito e Cidadania (AMC), formada em 2021, surgiu como um dos produtos das ações desempenhadas ao longo dos anos, sendo mais um apoio para o desenvolvimento dos projetos. Dessa forma, a AMC passou a ter, pela primeira vez, um setor voltado exclusivamente para a gestão da política cicloviária.",
-        "De 2013 até 2023, houve um aumento expressivo no alcance da malha cicloviária, evoluindo de 68,8 para 428,8 km. Outros avanços foram o sistema de bicicletas compartilhadas (Bicicletar, Mini Bicicletar e Bicicletar Corporativo), estruturas de estacionamento como bicicletários e paraciclos, atividades como Ciclofaixa de Lazer e Bike Sem Barreiras, além do monitoramento de sinistros de trânsito envolvendo bicicletas e coleta de dados pela AMC.",
+        "De 2013 até 2025, houve um aumento expressivo no alcance da malha cicloviária, evoluindo de 68,8 para 501,7 km. Outros avanços foram o sistema de bicicletas compartilhadas (Bicicletar, Mini Bicicletar e Bicicletar Corporativo), estruturas de estacionamento como bicicletários e paraciclos, atividades como Ciclofaixa de Lazer e Bike Sem Barreiras, além do monitoramento de sinistros de trânsito envolvendo bicicletas e coleta de dados pela AMC.",
         "Mais sobre a política cicloviária",
         "Essa sequência de mudanças colocou Fortaleza em destaque na América Latina, tornando-se uma das cidades que mais usam bicicleta. Contudo, alguns obstáculos ainda precisam ser enfrentados, principalmente no sentido de alcançar públicos diferentes. Para isso, a Prefeitura de Fortaleza segue desenvolvendo ações junto à Bloomberg Initiative for Cycling Infrastructure (BICI), financiada pela Bloomberg Philanthropies em parceria com a Global Designing Cities Initiative (GDCI), visando dar mais conforto e proteção à malha cicloviária da cidade.",
         "Mas os avanços neste novo ciclo de renovação de olhares e perspectivas não param. Outras iniciativas estão em andamento e poderão ser acompanhadas por aqui.",
